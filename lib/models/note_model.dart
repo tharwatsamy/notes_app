abccd4eb7fcdc600365757fda9661f5d7d279e3d
tharwatsamy <tharwat.s@clouds.com.kw>
2022-11-06 00:43:39 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+}
