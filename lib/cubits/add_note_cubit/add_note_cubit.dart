f0ad4f3cbd87754e8e6adef17bdd3c663f19b0f8
tharwatsamy <tharwat.s@clouds.com.kw>
2022-11-21 04:20:09 +0200

    create add note cubit

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'add_note_state.dart';
+
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitial());
+}
---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,6 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+abstract class AddNoteState {}
+
+class AddNoteInitial extends AddNoteState {}
