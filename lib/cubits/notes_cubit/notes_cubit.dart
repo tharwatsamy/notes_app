4d32e3524abe8d749dbd15c36c272314f58822f4
tharwatsamy <tharwat.s@clouds.com.kw>
2022-11-24 02:09:29 +0200

    create notes state

---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+}
---
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -0,0 +1,20 @@
+part of 'notes_cubit.dart';
+
+@immutable
+abstract class NotesState {}
+
+class NotesInitial extends NotesState {}
+
+class NotesLoading extends NotesState {}
+
+class NotesSuccess extends NotesState {
+  final List<NoteModel> notes;
+
+  NotesSuccess(this.notes);
+}
+
+class NotesFailure extends NotesState {
+  final String errMessage;
+
+  NotesFailure(this.errMessage);
+}
